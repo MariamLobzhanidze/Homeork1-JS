b77b17f1776388b9c6ae6a1c29a48677d8aa639c
Mariam Lobzhanidze <163752402+MariamLobzhanidze@users.noreply.github.com>
2024-04-25 10:19:39 +0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var userName = "David";
+let age = 32;
+const hasHobbies = true;
+console.log("Hello, I am " + userName + " and I am " + age + " years old.");
+userName = "Daviti";
+age = 33;
+
+console.log(
+  "Hello, I am " + " " + " " + userName + " and I am " + age + " years old."
+);
+console.log(`Hello, I am  ${userName} and I am ${age} years old.`);
+
+console.log(typeof userName);
+console.log(typeof age);
+
