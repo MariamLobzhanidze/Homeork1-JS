62084d6d196f64c026e06e4969e98c03ff0ae0dd
Mariam Lobzhanidze <163752402+MariamLobzhanidze@users.noreply.github.com>
2024-04-25 10:26:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ console.log(`Hello, I am  ${userName} and I am ${age} years old.`);
 console.log(typeof userName);
 console.log(typeof age);
 
+
+
+let x = 115;
+let y = 45;
+let z = 60;
+
+let sum = x + y + z
+console.log(sum);
+
+let a = 400;
+let b = 70;
+let c = 20;
+
+let sum = a - b - c
+console.log(sum);
+
+let d = 80;
+let e = 4;
+
+let sum = 80 / 4 
+
+let k = 800
+let m = 9
+
+let sum = k * m
+
+
+
